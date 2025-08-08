94aab3a524ab5916aa767a53fb355c24b80e0442
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-08-08 15:15:49 +0530

    Time: 2 ms (77.25%), Space: 41.7 MB (85.63%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.java
+++ 0826-soup-servings/0826-soup-servings.java
@@ -8,16 +8,16 @@ class Solution {
             return dp[a][b];
         }
         double ans=0;
-        ans+=0.25*soup(a-100,b-0,dp);
-        ans+=0.25*soup(a-75,b-25,dp);
-        ans+=0.25*soup(a-50,b-50,dp);
-        ans+=0.25*soup(a-25,b-75,dp);
+        ans+=0.25*soup(a-4,b-0,dp);
+        ans+=0.25*soup(a-3,b-1,dp);
+        ans+=0.25*soup(a-2,b-2,dp);
+        ans+=0.25*soup(a-1,b-3,dp);
         dp[a][b]=ans;
         return ans;
     } 
     public double soupServings(int n) {
         if(n>5000) return 1.0;
-        //n=(int) Math.ceil(n/25.0);
+        n=(int) Math.ceil(n/25.0);
         double[][] dp=new double[n+1][n+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=n;j++){
