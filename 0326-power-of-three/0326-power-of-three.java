affd68a65aa4f3c64b7fb0897013de342f748eb8
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-08-13 09:50:52 +0530

    Time: 8 ms (84.41%), Space: 44.2 MB (51.08%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPowerOfThree(int n) {
         if(n==0) return false;
         if(n==1) return true;
-        if(n%3!=0) return false;
-      return isPowerOfThree(n/3); 
+        if(n%3==0) return isPowerOfThree(n/3); 
+      return false;
     }
 }
