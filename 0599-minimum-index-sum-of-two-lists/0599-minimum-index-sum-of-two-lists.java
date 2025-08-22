ac1ec6356a38b39a30c176590c6e52b04127eb2c
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-08-22 23:10:15 +0530

    Time: 106 ms (20.78%), Space: 45.5 MB (65.64%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,28 +1,36 @@
 class Solution {
     public String[] findRestaurant(String[] l1, String[] l2) {
-         Map<String,Integer> mp=new HashMap<>(); 
-        for(int i=0;i<l1.length;i++){
-            mp.put(l1[i],i);
+        List<String> l = new ArrayList<>();
+        for (int i = 0; i < l1.length; i++) {
+            l.add(l1[i]);
         }
-        int min=Integer.MAX_VALUE;
-       List<String> res=new ArrayList<>();
-        for(int i=0;i<l2.length;i++){
-            if(mp.containsKey(l2[i])){
-                int j=mp.get(l2[i]);
-                int sum=i+j;
-                if(sum<min){
-                    res.clear();
-                    res.add(l2[i]);
-                    min=sum;
-                }
-                else if(sum==min){
-                    res.add(l2[i]);
-                    min=sum;
+
+        int min = Integer.MAX_VALUE;
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < l2.length; i++) {
+            if (l.contains(l2[i])) {
+                for (int j = 0; j < l1.length; j++) {
+                    if (l2[i].equals(l1[j])) {   
+                        if (min > i + j) {
+                            ans.clear();         
+                            ans.add(l1[j]);
+                            min = i + j;
+                        } else if (min == i + j) {
+                            ans.add(l1[j]);     
+                        }
+                        break;
+                    }
                 }
             }
         }
 
-           return res.toArray(new String[0]); 
-        
+        String[] str = new String[ans.size()];
+        int idx = 0;
+        for (String s : ans) {
+            str[idx++] = s;
+        }
+
+        return str;
     }
 }
