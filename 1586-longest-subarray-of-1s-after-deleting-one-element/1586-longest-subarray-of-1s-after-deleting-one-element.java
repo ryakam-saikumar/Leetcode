9505b0500a66cfd514d6642a0d26de9dc428128d
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-08-24 10:58:15 +0530

    Time: 2 ms (98.24%), Space: 57 MB (48.74%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -7,19 +7,18 @@ class Solution {
         for(int i=0;i<n-1;i++){
             if(nums[i]==0){
                 zero=true;
-                int left=0;
-                int right=0;
+                int count=0;
                 int l=i-1;
                 while(l>=0&&nums[l]==1){
-                    right++;
+                    count++;
                     l--;
                 }
                 int r=i+1;
                 while(r<n&&nums[r]==1){
-                    right++;
+                    count++;
                     r++;
                 }
-                c=Math.max(c,left+right);
+                c=Math.max(c,count);
             }
 
             
