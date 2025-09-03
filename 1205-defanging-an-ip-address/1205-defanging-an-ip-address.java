1d78bf059e08c9cbc260fdfdf953e10ceedd7fc6
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-09-03 20:16:11 +0530

    Time: 0 ms (100%), Space: 41.5 MB (75.26%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='.'){
+                sb.append("[");
+                sb.append(".");
+                sb.append("]");
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
