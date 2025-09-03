b1bb5fa2211a3a70a2477fa8b2d46be9f7152a0e
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-09-03 20:16:30 +0530

    Time: 0 ms (100%), Space: 42 MB (14.39%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -3,9 +3,8 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='.'){
-                sb.append("[");
-                sb.append(".");
-                sb.append("]");
+                sb.append("[.]");
+                
             }
             else{
                 sb.append(s.charAt(i));
