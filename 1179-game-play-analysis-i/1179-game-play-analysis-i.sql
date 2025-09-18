45c4c1ffdce24c2e8d203824f33414a92169bcfa
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-09-18 15:30:33 +0530

    Time: 474 ms (72%), Space: 0B (100%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -1,6 +1,1 @@
-SELECT a1.player_id, a1.event_date AS first_login
-FROM Activity a1
-LEFT JOIN Activity a2
-  ON a1.player_id = a2.player_id
- AND a1.event_date > a2.event_date
-WHERE a2.event_date IS NULL;
+select player_id,MIN(event_date) as first_login from Activity group by player_id;
