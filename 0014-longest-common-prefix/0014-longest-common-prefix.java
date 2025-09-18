72f10d5a0d40447ef23dfc53562d8b3c9ddda500
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-09-18 12:50:34 +0530

    Time: 8 ms (7.98%), Space: 42.2 MB (9.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,49 +1,18 @@
-import java.lang.*;
-class TrieNode{
-    TrieNode[] children=new TrieNode[26];
-    int c;
-    TrieNode(){
-        Arrays.fill(children,null);
-        c=0;
-    }
-}
-class Trie{
-    TrieNode root;
-    Trie(){
-        root=new TrieNode();
-    }
-    public void insert(String str){
-        TrieNode node=root;
-        for(char ch:str.toCharArray()){
-            if(node.children[ch-'a']==null){
-                node.children[ch-'a']=new TrieNode();
-            }
-            node=node.children[ch-'a'];
-node.c++;
-        }
-         
-    }
-    public String search(String s,int n){
-        TrieNode node=root;
-        StringBuilder sb=new StringBuilder();
-        for(char ch:s.toCharArray()){
-            if(node.children[ch-'a'].c!=n)
-            {
-                return sb.toString();
-            }
-            node=node.children[ch-'a'];
-            sb.append(ch);
-        }
-        return sb.toString();
-    }
-}
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-       Trie trie=new Trie();
-       for(String s:strs){
-        trie.insert(s);
-       }
-       String res=trie.search(strs[0],strs.length);
-       return res;
-    }
+        if(strs.length==0) return "";
+        Arrays.sort(strs);
+        
+        int i=0;
+        int j=0;
+        String s1=strs[0];
+        String sn=strs[strs.length-1];
+        String s="";
+        while(i<s1.length()&&j<sn.length()&&s1.charAt(i)==sn.charAt(j)){
+            s=s+s1.charAt(i);
+            i++;
+            j++;
+        }  
+        return s;  
+        }
 }
